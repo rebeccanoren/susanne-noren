30c82b569fa6369bd40b80d7c28cfbf9350201b8
rebeccanoren <hello@rebeccanoren.com>
2024-12-08 09:39:08 +0100

    update

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,8 +2,8 @@ import { createClient } from '$lib/prismicio';
 
 export const prerender = 'auto';
 
-export async function load({ fetch, cookies }) {
-	const client = createClient({ fetch, cookies });
+export async function load() {
+	const client = createClient();
 
 	const settings = await client.getSingle('settings');
 	const navigation = await client.getSingle('navigation');
